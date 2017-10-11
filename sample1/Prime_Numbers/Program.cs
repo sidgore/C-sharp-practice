f16c507d7de1316576ee264da7453cc4445b3a44
SIDDHARTH GORE <siddharth.gore@sjsu.edu>
2017-10-11 16:05:47 -0700

    Prime Number Program

---
+++ sample1/Prime_Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number!");
+
+
+            int n1;
+
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if(n1<2)
+                Console.WriteLine("Not a Prime number!");
+            if(n1==2)
+                Console.WriteLine("Its a prime number!");
+            if(n1>2)
+            {
+                int count = 0;
+                for (int i = 1; i <= n1; i++)
+                {
+                    if (n1 % i == 0)
+                    {
+
+                        count++;
+                    }
+                }
+                if(count==2)
+                {
+                    Console.WriteLine("Its a prime number!");
+                }
+                else Console.WriteLine("Not a Prime number!");
+            }
+
+                
+        }
+    }
+}
