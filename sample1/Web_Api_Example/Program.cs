000c645679d9b04150309b9dbbe32a21ef73c85b
sidgore <siddharth.gore@sjsu.edu>
2017-10-12 13:23:58 -0700

    * LinkedListExample.csproj: Linked List Example
    * Program.cs:

---
+++ sample1/Web_Api_Example/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        public class Node{
+
+            private int data;
+           public  Node next;
+
+           public Node()
+            {
+                data = 0;
+                next = null;
+                
+            }
+
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+
+
+            public void printNode()
+            {
+                Console.Write("|"+data+"|->");
+                if(next!=null)
+                {
+                    next.printNode();
+                }
+            }
+            public void addElementToLast(int d)
+            {
+
+                if(next==null)
+                {
+                    next = new Node(d); 
+                }
+
+                else
+                {
+                    next.addElementToLast(d);
+                }
+
+
+
+
+
+            }
+        }
+
+
+
+
+        static void Main(String[] args)
+        {
+
+            Node n = new Node(10);
+
+            n.addElementToLast(56);
+            n.addElementToLast(34);
+
+
+            n.printNode();
+
+
+           
+        }
+
+     
+    }
+
+
+
+
+ 
+}
